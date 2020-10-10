efc4ec7ec3a428dac6d354321519a4fa3a035167
Mohammad Tareq <mdtareq320@gmail.com>
2020-10-10 12:21:49 +0600

    new

--- js/product-menu.js
+++ js/product-menu.js
@@ -1,50 +1,18 @@
 const offBtn=document.getElementById('off-btn');
-// // const off=document.getElementById('off');
-// // offBtn.addEventListener('click',function(event){  
-// //     if(off.style.display ==='none'){
-// //         off.style.display = 'block';
-// //     }
-// //     else{
-// //         off.style.display = 'none';
-// //     }
+const closeBtn=document.getElementById('close-btn');
+offBtn.addEventListener('click',function(){  
+    offBtn.style.display='none';
+    closeBtn.style.display='block';
 
-// // })
+})
 
-// document.body.addEventListener('click', function (event) {
-//     const offBtn = document.getElementById('off-btn');
-//     const off = document.getElementById('off');
-//     const menu1 = document.getElementById('menu1');
-//     const menu2 = document.getElementById('menu2');
-//     const menu3 = document.getElementById('menu3');
-//     const menu4 = document.getElementById('menu4');
-//     const menu5 = document.getElementById('menu5');
-//     const menu6 = document.getElementById('menu6');
-//     const menu7 = document.getElementById('menu7');
-//     const menu8 = document.getElementById('menu8');
-//     const range = document.getElementById('range');
-//     if (event.target.parentNode === offBtn) {
-//         if (off.style.display === 'none') {
-//             off.style.display = 'block';
-//         }
-//         else {
-//             off.style.display = 'none';
-//         }
-//     }
-//     else{
-//         if(event.target.parentNode!==menu1 && event.target.parentNode!==menu2 &&
-//             event.target.parentNode!==menu3 && event.target.parentNode!==menu4 &&
-//             event.target.parentNode!==menu5 && event.target.parentNode!==menu6 &&
-//             event.target.parentNode!==menu7 && event.target.parentNode!==menu8 &&
-//             event.target!==menu1 && event.target!==menu2 &&
-//             event.target!==menu3 && event.target!==menu4 &&
-//             event.target!==menu5 && event.target!==menu6 &&
-//             event.target!==menu7 && event.target!==menu8 &&
-//             event.target.parentNode!==range && event.target!==range
-//             ){
-//             off.style.display = 'none';
-//         }
-//     }
-// })
+closeBtn.addEventListener('click',function(event){  
+  offBtn.style.display='block';
+  closeBtn.style.display='none';
+
+})
+
+
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
   }
