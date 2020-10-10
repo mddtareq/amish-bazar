5f0c5cc09eb267f92aba05019d8ed1494eb422f6
Mohammad Tareq <mdtareq320@gmail.com>
2020-10-10 20:45:13 +0600

    new

--- js/product-menu.js
+++ js/product-menu.js
@@ -1,17 +1,20 @@
-const offBtn=document.getElementById('off-btn');
-const closeBtn=document.getElementById('close-btn');
-const sideBar=document.getElementById('see-side');
-offBtn.addEventListener('click',function(){  
-    offBtn.style.display='none';
-    closeBtn.style.display='block';
-    sideBar.style.width='100%';
+const offBtn = document.getElementById('off-btn');
+const closeBtn = document.getElementById('close-btn');
+const sideBar = document.getElementById('see-side');
+const custom = document.getElementById('custom');
+offBtn.addEventListener('click', function () {
+  offBtn.style.display = 'none';
+  closeBtn.style.display = 'block';
+  sideBar.style.width = '100%';
+  sideBar.style.transition='1.5s';
+
 })
 
-closeBtn.addEventListener('click',function(event){  
-  offBtn.style.display='block';
-  closeBtn.style.display='none';
-  sideBar.style.width='0%';
-  sideBar.style.transition='0.2s';
+closeBtn.addEventListener('click', function (event) {
+  offBtn.style.display = 'block';
+  closeBtn.style.display = 'none';
+  sideBar.style.width = '0%';
+  sideBar.style.transition = '1s';
 })
 
 
@@ -20,8 +23,8 @@ const min = slider.min
 const max = slider.max
 const value = slider.value
 
-slider.style.background = `linear-gradient(to right, #FFC729 0%, #FFC729 ${(value-min)/(max-min)*100}%, #DEE2E6 ${(value-min)/(max-min)*100}%, #DEE2E6 100%)`
+slider.style.background = `linear-gradient(to right, #FFC729 0%, #FFC729 ${(value - min) / (max - min) * 100}%, #DEE2E6 ${(value - min) / (max - min) * 100}%, #DEE2E6 100%)`
 
-slider.oninput = function() {
-  this.style.background = `linear-gradient(to right, #FFC729 0%, #FFC729 ${(this.value-this.min)/(this.max-this.min)*100}%, #DEE2E6 ${(this.value-this.min)/(this.max-this.min)*100}%, #DEE2E6 100%)`
+slider.oninput = function () {
+  this.style.background = `linear-gradient(to right, #FFC729 0%, #FFC729 ${(this.value - this.min) / (this.max - this.min) * 100}%, #DEE2E6 ${(this.value - this.min) / (this.max - this.min) * 100}%, #DEE2E6 100%)`
 };
